1480e8b1d0fcf67ec16cc5fdec0e5b2635168c07
Андрей Власов <andrey11vlasov@yandex.ru>
2018-02-14 23:40:11 +0300

    Add project files.

---
+++ PermutationTest/Permute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationTest
+{
+    class Permute
+    {       
+        public static List<string> GetPermutation(string str)
+        {
+            var permutationResult = new List<string>();
+            PermutationAlgorithm(str.Length - 1, str.ToCharArray(), permutationResult);
+            permutationResult.Sort();
+            return permutationResult;
+        }
+
+        static void PermutationAlgorithm(int n, char[] str, List<string> permutationResult)
+        {
+            if(n == 0)
+            {                
+                permutationResult.Add(new string(str));
+            }
+            else
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    PermutationAlgorithm(n - 1, str, permutationResult);
+                    Swap(str, n % 2 == 0 ? i : 0, n);
+                }
+            }
+        }
+
+        static void Swap(char[]str, int i, int j)
+        {
+            var temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+    }
+}
---
+++ PermutationTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string str = "abcd";
+            List<string> permutations = Permute.GetPermutation(str);
+            Print(permutations);
+            Console.ReadKey();
+        }
+
+        static void Print<T>(IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
